943bb466115820d8511a72e146503f647527be2c
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-28 16:00:00 -0400

    add User class tests

--- tests/User.test.js
+++ tests/User.test.js
@@ -6,13 +6,45 @@ const user = new User('Joe Bloggs', 'test123', 21)
 describe('User property tests', () => {
   // test username
   test('username should be a string', () => {
-    expect(typeof user.username).toBe('string')
+    expect(typeof user._username).toBe('string')
   })
   // test password
 
+  test('password sgould be a string', () => {
+    expect(typeof user._password).toBe('string')
+  })
   // test age
+  test('age should be an integer', () => {
+    expect(typeof user._age).toBe('number')
+  })
+
+  test('loggedIn shuld be a boolean', () =>{
+    expect(typeof user._loggedIn).toBe('boolean')
+  })
 })
 
-// test login
 
-// test logout
+describe('User method tests', () => {
+  let user;
+
+  beforeEach(() => {
+    user = new User('Jane Magazzi', 'test123', 31);
+  });
+
+  test('login with correct password should set loggedIn to true', () => {
+    user.login('Jane Magazzi', 'test123');
+    expect(user._loggedIn).toBe(true);
+  });
+
+  test('login with incorrect password should throw an error', () => {
+    expect(() => {
+      user.login('Jane Magazzi', 'wrongpassword');
+    }).toThrow('Username or password is incorrect');
+  });
+
+  test('logout should set loggedIn to false', () => {
+    user.login('Jane Magazzi', 'test123');
+    user.logout();
+    expect(user._loggedIn).toBe(false);
+  });
+});
