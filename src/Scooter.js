bef6826103047df5f2cab8ffe6b9691ba1b1f90b
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-28 15:59:33 -0400

    add Scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,43 @@
 class Scooter {
-  // scooter code here
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+  async rent(user) {
+    if (this.charge > 20 && !this.isBroken) {
+      this.station = null;
+      this.user = user;
+    } else {
+      throw new Error('Scooter needs to charge or needs repair.');
+    }
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
+  async recharge() {
+    console.log('Starting charge');
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    this.charge = 100;
+
+    console.log('Charge complete');
+  }
+
+  async requestRepair() {
+    console.log('Requesting repair');
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    this.isBroken = false;
+
+    console.log('Repair completed');
+  }
 }
 
-module.exports = Scooter
+module.exports = Scooter;
