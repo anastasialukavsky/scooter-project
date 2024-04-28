0e32ae507885678625de7547bf280f1d8ab15e2e
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-28 15:59:51 -0400

    add Scooters class tests

--- src/User.js
+++ src/User.js
@@ -1,5 +1,22 @@
 class User {
-  // User code here
+  constructor(username, password, age) {
+    this._username = username;
+    this._password = password;
+    this._age = age;
+    this._loggedIn = false;
+  }
+
+  login(username, password) {
+    if (username === this._username && password === this._password) {
+      this._loggedIn = true;
+    } else {
+      throw new Error('Username or password is incorrect');
+    }
+  }
+
+  logout() {
+    this._loggedIn = false;
+  }
 }
 
 module.exports = User
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,4 +1,5 @@
 const Scooter = require('../src/Scooter')
+const User = require('../src/User')
 
 // typeof scooter === object
 describe('scooter object', () => {
@@ -13,11 +14,35 @@ describe('scooter methods', () => {
   // tests here!
 
   // rent method
-
+  test('rent scooter when charged and not broken', async () => {
+    const scooter = new Scooter('Station A');
+    const user = new User('testuser', 'password', 20);
+    await scooter.rent(user);
+    expect(scooter.user).toBe(user);
+  });
   // dock method
 
   // requestRepair method
 
   // charge method
+  test('dock scooter', () => {
+    const scooter = new Scooter('Station A');
+    scooter.dock('Station B');
+    expect(scooter.station).toBe('Station B');
+    expect(scooter.user).toBe(null);
+  });
+
+  test('recharge scooter', async () => {
+    const scooter = new Scooter('Station A');
+    await scooter.recharge();
+    expect(scooter.charge).toBe(100);
+  });
+
+  test('request repair', async () => {
+    const scooter = new Scooter('Station A');
+    scooter.isBroken = true;
+    await scooter.requestRepair();
+    expect(scooter.isBroken).toBe(false);
+  });
 
 })
