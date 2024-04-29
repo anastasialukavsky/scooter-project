4b57178f526d066412128b76f7a310edababe62f
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-29 12:49:54 -0400

    add logout gueard against user who's not logged in test

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,28 +1,27 @@
-const User = require('../src/User')
+const User = require('../src/User');
 
-const user = new User('Joe Bloggs', 'test123', 21)
+const user = new User('Joe Bloggs', 'test123', 21);
 
 // User tests here
 describe('User property tests', () => {
   // test username
   test('username should be a string', () => {
-    expect(typeof user._username).toBe('string')
-  })
+    expect(typeof user._username).toBe('string');
+  });
   // test password
 
   test('password sgould be a string', () => {
-    expect(typeof user._password).toBe('string')
-  })
+    expect(typeof user._password).toBe('string');
+  });
   // test age
   test('age should be an integer', () => {
-    expect(typeof user._age).toBe('number')
-  })
+    expect(typeof user._age).toBe('number');
+  });
 
-  test('loggedIn shuld be a boolean', () =>{
-    expect(typeof user._loggedIn).toBe('boolean')
-  })
-})
-
+  test('loggedIn shuld be a boolean', () => {
+    expect(typeof user._loggedIn).toBe('boolean');
+  });
+});
 
 describe('User method tests', () => {
   let user;
@@ -46,5 +45,11 @@ describe('User method tests', () => {
     user.login('Jane Magazzi', 'test123');
     user.logout();
     expect(user._loggedIn).toBe(false);
+  });
+  
+  test('logout when not logged in should throw an error', () => {
+    expect(() => {
+      user.logout();
+    }).toThrow('User is not logged in');
   });
 });
